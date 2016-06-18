72ceec6b5ea3acab2b4d2298ca21c2294f39faf3
kable <kablefly2014@163.com>
2016-06-18 14:18:35 +0800

    include index.js and Hello Component

---
+++ src/js/Hello.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Hello = React.createClass({
+	render: function(){
+		return (
+			<h1>您好啊，Kable先生!</h1>
+		);
+	}
+});
+
+module.exports = Hello;
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var Hello = require('./Hello.js');
+
+ReactDom.render(<Hello />,document.getElementById('app'));
